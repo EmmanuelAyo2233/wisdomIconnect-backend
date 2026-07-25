88682c8d0e73ec3058fbbbd7e2ca79ba00a53359
EmmanuelAyo2233 <emmanuelayobami61@gmail.com>
2026-07-26 00:09:35 +0100

    git add .

--- controllers/callSocketController.js
+++ controllers/callSocketController.js
@@ -2,51 +2,48 @@ exports.setupCallSocket = (io) => {
     io.of("/chat").on("connection", (socket) => {
 
         socket.on("joinRoom", ({ meetingId, userId, role }) => {
-            socket.userId = String(userId);
-            socket.meetingId = meetingId;
-            socket.role = role;
             socket.join(meetingId);
-            socket.to(meetingId).emit("userJoined", { userId, role, socketId: socket.id });
+            socket.to(meetingId).emit("userJoined", { userId, role });
         });
 
         socket.on("offer", ({ meetingId, offer }) => {
-            socket.to(meetingId).emit("offer", { offer, senderUserId: socket.userId });
+            socket.to(meetingId).emit("offer", offer);
         });
 
         socket.on("answer", ({ meetingId, answer }) => {
-            socket.to(meetingId).emit("answer", { answer, senderUserId: socket.userId });
+            socket.to(meetingId).emit("answer", answer);
         });
 
         socket.on("ice-candidate", ({ meetingId, candidate }) => {
-            socket.to(meetingId).emit("ice-candidate", { candidate, senderUserId: socket.userId });
+            socket.to(meetingId).emit("ice-candidate", candidate);
         });
 
         socket.on("endCall", ({ meetingId }) => {
             // Broadcast only to OTHER participants in the room, not back to the caller
-            socket.to(meetingId).emit("callEnded", { senderUserId: socket.userId });
+            socket.to(meetingId).emit("callEnded");
         });
 
         socket.on("forceMute", ({ meetingId }) => {
-            socket.to(meetingId).emit("forceMute", { senderUserId: socket.userId });
+            socket.to(meetingId).emit("forceMute");
         });
 
         socket.on("forceVideoOff", ({ meetingId }) => {
-            socket.to(meetingId).emit("forceVideoOff", { senderUserId: socket.userId });
+            socket.to(meetingId).emit("forceVideoOff");
         });
 
         // Relay media state changes (camera/mic/screen share toggle)
         socket.on("mediaStateChanged", ({ meetingId, ...state }) => {
-            socket.to(meetingId).emit("mediaStateChanged", { ...state, senderUserId: socket.userId });
+            socket.to(meetingId).emit("mediaStateChanged", state);
         });
 
         // Relay hand raising event
         socket.on("raiseHand", ({ meetingId, userId, isRaised }) => {
-            socket.to(meetingId).emit("raiseHand", { userId, isRaised, senderUserId: socket.userId });
+            socket.to(meetingId).emit("raiseHand", { userId, isRaised });
         });
 
         // Relay emoji reactions
         socket.on("emojiReaction", ({ meetingId, userId, emoji }) => {
-            socket.to(meetingId).emit("emojiReaction", { userId, emoji, senderUserId: socket.userId });
+            socket.to(meetingId).emit("emojiReaction", { userId, emoji });
         });
 
         // In-call chat message
@@ -63,7 +60,7 @@ exports.setupCallSocket = (io) => {
         socket.on("disconnecting", () => {
             for (const room of socket.rooms) {
                 if (room !== socket.id) {
-                    socket.to(room).emit("userLeft", { reason: "disconnected", socketId: socket.id });
+                    socket.to(room).emit("userLeft", { reason: "disconnected" });
                 }
             }
         });
@@ -73,4 +70,5 @@ exports.setupCallSocket = (io) => {
         });
     });
 };
+
 
